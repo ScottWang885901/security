f87c4bb13c3f3f7bbe85b5f174da3e97191fb0df
wangbo <scott_iso8859_1@163.com>
2019-11-19 10:48:54 +0800

    Spring-Security pom 文件修改

---
+++ security-app/src/main/java/com/scott/study/App.java
@@ -0,0 +1,13 @@
+package com.scott.study;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName App
+ * @Description TODO
+ * @Author swang25
+ * @Date 2019/11/18  17:20
+ */
+@SpringBootApplication
+public class App {
+}
--- security-demo/src/main/java/com/scott/study/App.java
+++ security-demo/src/main/java/com/scott/study/App.java
@@ -1,7 +1,12 @@
 package com.scott.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
-
     public static void main(String[] args) {
-
+        SpringApplication springApplication = new  SpringApplication();
+        springApplication.run(App.class,args);
     }
 }
